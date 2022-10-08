168d76630b1827c2e2f534f00cb1b4b1d721793a
heppocogne <83043568+heppocogne@users.noreply.github.com>
2022-10-08 11:58:24 +0900

    pick svg files on creating _rawsvg folder

--- addons/svg_sprite/plugin.gd
+++ addons/svg_sprite/plugin.gd
@@ -17,6 +17,7 @@ func _enter_tree():
 		var dir:=Directory.new()
 		if !dir.dir_exists(_rawsvg_root):
 			dir.make_dir(_rawsvg_root)
+			pick_svg_files("res://")
 	
 	add_custom_type("SVGSprite","Node2D",preload("res://addons/svg_sprite/svgsprite_library.gdns"),preload("res://addons/svg_sprite/icon_svgsprite.svg"))
 
@@ -28,6 +29,27 @@ func _exit_tree():
 		res_file_sys.disconnect("resources_reimported",self,"_on_resources_reimported")
 		file_sys.disconnect("file_removed",self,"_on_file_removed")
 		file_sys.disconnect("files_moved",self,"_on_files_moved")
+
+
+func pick_svg_files(start_path:String):
+	var dir:=Directory.new()
+	dir.change_dir(start_path)
+	dir.list_dir_begin(true,true)
+	var item_name:=dir.get_next()
+	var svg_files:PoolStringArray=[]
+	
+	while item_name!="":
+		if item_name==".gdignore":
+			break
+		var item_path:=dir.get_current_dir().plus_file(item_name)
+		if dir.dir_exists(item_path):
+			pick_svg_files(item_path)
+		elif dir.file_exists(item_path) and item_name.ends_with(".svg"):
+			svg_files.append(item_path)
+		item_name=dir.get_next()
+	dir.list_dir_end()
+	
+	_on_resources_reimported(svg_files)
 
 
 func get_rawsvg_path(path:String)->String:
