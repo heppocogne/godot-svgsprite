6d6ee2f5dc599fc3821e42dde1c331d617e854c6
heppocogne <83043568+heppocogne@users.noreply.github.com>
2023-01-11 14:33:47 +0900

    Update demo

--- demo/demo.gd
+++ demo/demo.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var svg_sprite:SVGSprite=$SVGSprite
 onready var svg_sprite2:SVGSprite=$SVGSprite2
+onready var svg_sprite3:SVGSprite=$SVGSprite3
 
 
 func _ready():
@@ -12,3 +13,4 @@ func _process(delta:float):
 	rotation+=delta
 	svg_sprite.rotation-=2.0*delta
 	svg_sprite2.scale=(2.0+sin(rotation))*Vector2(0.25,0.25)
+	svg_sprite3.rotation+=2.0*delta
